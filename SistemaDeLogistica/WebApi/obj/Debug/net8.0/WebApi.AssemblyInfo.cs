d7558986d65a35847d1434af8e9e549ad650f5c1
Nahuel Adrian Krauss <130124513+nahuk86@users.noreply.github.com>
2025-05-20 19:04:41 -0300

    pruebas

--- SistemaDeLogistica/WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
+++ SistemaDeLogistica/WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f378acd65d35a788c225f25e99a74171182b933")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e1dfc6c4bd1964c9c9a6de90805c026833bfd42")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
